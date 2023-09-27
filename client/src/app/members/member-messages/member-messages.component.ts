e12bed54d51562f96596606f1b84da5a58398eac
derekdreibrodt <taylorbendtransport@wdwebsolutions-email.com>
2023-09-26 19:47:34 -0500

    193 - Styled messages thread page

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
+import { TimeagoClock, TimeagoModule } from 'ngx-timeago';
 import { Message } from 'src/app/_models/message';
 import { MessagesService } from 'src/app/_services/messages.service';
 
@@ -8,7 +9,7 @@ import { MessagesService } from 'src/app/_services/messages.service';
   standalone: true,
   templateUrl: './member-messages.component.html',
   styleUrls: ['./member-messages.component.css'],
-  imports: [CommonModule]
+  imports: [CommonModule, TimeagoModule]
 })
 export class MemberMessagesComponent implements OnInit {
   @Input() username?: string;
