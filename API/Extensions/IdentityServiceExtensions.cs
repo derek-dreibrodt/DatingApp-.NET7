d5008100fa3cd19800dcee53d042101f17a99960
derekdreibrodt <taylorbendtransport@wdwebsolutions-email.com>
2023-10-14 18:40:55 -0500

    204 - Configured Identity Service Extension (startup)

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -3,16 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
 
 namespace API.Extensions
 {
     public static class IdentityServiceExtensions
     {
+        
+
         public static void AddIdentityServices(this IServiceCollection services, 
         IConfiguration config)
         {
+            services.AddIdentityCore<AppUser>(opt => 
+            {
+                opt.Password.RequireNonAlphanumeric = false;
+                //opt.User.RequireUniqueEmail;
+            })
+            .AddRoles<AppRole>()
+            .AddRoleManager<RoleManager<AppRole>>() // Configures RoleManager service
+            .AddEntityFrameworkStores<DataContext>(); // Creates all the tables related to identity in our database
+
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             .AddJwtBearer (options =>
             {
